9444c18a035670e508f987aa5ee0c46ff92aa722
Muhammad Usman <usman.1372@gmail.com>
2024-12-04 22:33:11 +0500

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Muhammad Usman on 04/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
